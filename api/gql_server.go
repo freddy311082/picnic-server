752c6dff7fec716ca98e4f586212122e02a95361
Freddy Martinez <freddy.martinez@autodesk.com>
2020-03-27 17:28:58 -0300

    First webserver version implemented
    TODO: test graphql server

--- api/gql_server.go
+++ api/gql_server.go
@@ -2,8 +2,16 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/freddy311082/picnic-server/service"
+	"github.com/freddy311082/picnic-server/utils"
+	"github.com/graphql-go/graphql"
 	"net/http"
 )
+
+type reqBody struct {
+	Query string `json:"query"`
+}
 
 type WebServer interface {
 	Start()
@@ -13,28 +21,49 @@ type WebServer interface {
 type gqlServerImp struct {
 }
 
-func (server *gqlServerImp) processQuery(query string) string {
+func (server *gqlServerImp) Start() {
+	portStr := fmt.Sprintf(":%d", service.SettingsObj().APISettings().HttpPort())
 
+	http.Handle("/graphql", server.getGqlHandler())
+	http.ListenAndServe(portStr, nil)
 }
 
-func (server *gqlServerImp) Start() {
-	http.Handle("/graphql", http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
+func (server *gqlServerImp) getGqlHandler() http.HandlerFunc {
+	return http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
 		if request.Body == nil {
 			return
 		}
 
-		type reqBody struct {
-			Query string `json:"query"`
-		}
+		rBody := server.decodeRequest(request, response)
+		server.processQuery(rBody.Query)
+	})
+}
 
-		var rBody reqBody
-		if err := json.NewDecoder(request.Body).Decode(&rBody); err != nil {
-			const msg = "Error parsing JSON request body"
-			http.Error(response, msg, 400)
-		}
+func (server *gqlServerImp) decodeRequest(request *http.Request, response http.ResponseWriter) reqBody {
+	var rBody reqBody
+	if err := json.NewDecoder(request.Body).Decode(&rBody); err != nil {
+		const msg = "Error parsing JSON request body"
+		utils.PicnicLog_ERROR(msg)
+		http.Error(response, msg, 400)
+	}
+	return rBody
+}
 
-		server.processQuery(response, rBody.Query)
-	}))
+func (server *gqlServerImp) processQuery(query string) (string, error) {
+	schema, err := GetSchema()
+
+	if err != nil {
+		return "", err
+	}
+
+	gqlParams := graphql.Params{
+		Schema:        *schema,
+		RequestString: query,
+	}
+
+	result := graphql.Do(gqlParams)
+	responseJSON, _ := json.Marshal(result)
+	return fmt.Sprintf("%s", responseJSON), nil
 }
 
 func (server *gqlServerImp) Stop() {
--- dbmanager/mongo_test.go
+++ dbmanager/mongo_test.go
@@ -1,7 +1,6 @@
 package dbmanager
 
 import (
-	"github.com/freddy311082/picnic-server/model"
 	"github.com/freddy311082/picnic-server/service"
 	"testing"
 )
@@ -22,21 +21,6 @@ func TestOpenConnectionWithCloudMongo(t *testing.T) {
 	if mongodbManager, err := initMongodbManagerForTesting(); err != nil {
 		t.Error(err)
 	} else {
-		mongodbManager.Close()
-	}
-}
-
-func TestRegisterNewUser(t *testing.T) {
-	if mongodbManager, err := initMongodbManagerForTesting(); err != nil {
-		t.Error(err)
-	} else {
-		mongodbManager.RegisterNewUser(&model.User{
-			ID:       "",
-			Name:     "",
-			LastName: "",
-			Email:    "",
-			Token:    "",
-		})
 		mongodbManager.Close()
 	}
 }
